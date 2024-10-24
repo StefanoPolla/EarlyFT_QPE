d694bebbf4d4c215708c8558a6b939fe1f30b271
alicjadut <37734788+alicjadut@users.noreply.github.com>
2024-10-24 14:58:22 +0200

    change order

--- eftqpe/MSQPE.py
+++ eftqpe/MSQPE.py
@@ -12,6 +12,80 @@ DEPTH_FACTOR_2 = 1.0
 # Max number of samples in the intermediate regime used for calculating the threshold error
 MAX_NSHOT = 100
 
+def OptMLESinQPE_params(target_error, damp_strength=0.0, grid_search_width=5):
+    thresh_dim1 = int(DEPTH_FACTOR_1 * damp_strength ** (-1 / 3)) + 1
+    thresh_dim2 = int(DEPTH_FACTOR_2 / damp_strength) + 1
+
+    thresh_error1 = SinQPE_Holevo_error(thresh_dim1, damp_strength)
+    thresh_error2 = 1 / np.sqrt(SinQPE_FI(thresh_dim2, damp_strength) * MAX_NSHOT)
+
+    if target_error > thresh_error1:
+        n_samples = 1
+        control_dim = 1 + np.ceil(np.pi / target_error).astype(int)
+    elif target_error < thresh_error2:
+        n_samples = np.ceil(1 / SinQPE_FI(thresh_dim2, damp_strength) / target_error**2).astype(int)
+        control_dim = thresh_dim2
+    else:
+        control_dim, n_samples = _OptMLESinQPE_midregime(
+            target_error,
+            damp_strength,
+            thresh_dim1,
+            thresh_dim2,
+            grid_search_width=grid_search_width,
+        )
+
+    return int(control_dim), int(n_samples)
+
+def _OptMLESinQPE_midregime(
+    target_error, damp_strength, thresh_dim1, thresh_dim2, initial_guess=None, grid_search_width=5
+):
+    if initial_guess is None:
+        initial_guess = [(thresh_dim1 + thresh_dim2) / 2, 10]
+
+    def constraint(x):
+        control_dim, n_sample = x
+        return _continuous_error(control_dim, n_sample, damp_strength) - target_error
+
+    res = minimize(
+        _cost,
+        initial_guess,
+        bounds=[(thresh_dim1 // 3, thresh_dim2), (1, np.infty)],
+        constraints={"type": "eq", "fun": constraint},
+    )
+
+    if not res["success"]:
+        print(
+            f"Warning: optimization unsuccessful for target error {target_error}, damping strength {damp_strength}."
+        )
+        print(res)
+
+    control_dim_float, n_samples_float = res["x"]
+
+    control_dim, n_samples = _grid_minimization(
+        _cost, (control_dim_float, n_samples_float), grid_search_width, constraint
+    )
+
+    return control_dim, n_samples
+
+# Model specific functions
+
+def loglikelihood(samples, control_dim, damp_strength):
+    return lambda x: np.mean(
+        np.log(SinQPE_prob_func(np.array(samples), x, control_dim, damp_strength))
+    )
+
+def negloglikelihood(samples, control_dim, damp_strength):
+    return lambda x: -loglikelihood(samples, control_dim, damp_strength)(x)
+
+def MLESinQPE_var_model(damp_strength, control_dim, n_samples):
+    a = -np.log(1 - np.exp(-damp_strength * (control_dim - 1))) / 2
+    fail_prob = np.exp(-a * n_samples)
+    FI = SinQPE_FI(int(control_dim), damp_strength)
+    if n_samples == 0:
+        return fail_prob * 2
+    return fail_prob * 2 + (1 - fail_prob) * 1 / FI / n_samples
+
+# Optimization
 
 def bruteforce_minimize(f, N):
     ests = []
@@ -25,15 +99,6 @@ def bruteforce_minimize(f, N):
         vals.append(res["fun"])
     est = ests[np.argmin(vals)]
     return est
-
-def MLESinQPE_var_model(damp_strength, control_dim, n_samples):
-    a = -np.log(1 - np.exp(-damp_strength * (control_dim - 1))) / 2
-    fail_prob = np.exp(-a * n_samples)
-    FI = SinQPE_FI(int(control_dim), damp_strength)
-    if n_samples == 0:
-        return fail_prob * 2
-    return fail_prob * 2 + (1 - fail_prob) * 1 / FI / n_samples
-
 
 def _continuous_error(control_dim: float, n_sample: float, damp_strength):
     int_ctrldim = int(control_dim)
@@ -68,58 +133,5 @@ def _grid_minimization(fun, x0, width, constraint):
     return x
 
 
-def _OptMLESinQPE_midregime(
-    target_error, damp_strength, thresh_dim1, thresh_dim2, initial_guess=None, grid_search_width=5
-):
-    if initial_guess is None:
-        initial_guess = [(thresh_dim1 + thresh_dim2) / 2, 10]
-
-    def constraint(x):
-        control_dim, n_sample = x
-        return _continuous_error(control_dim, n_sample, damp_strength) - target_error
-
-    res = minimize(
-        _cost,
-        initial_guess,
-        bounds=[(thresh_dim1 // 3, thresh_dim2), (1, np.infty)],
-        constraints={"type": "eq", "fun": constraint},
-    )
-
-    if not res["success"]:
-        print(
-            f"Warning: optimization unsuccessful for target error {target_error}, damping strength {damp_strength}."
-        )
-        print(res)
-
-    control_dim_float, n_samples_float = res["x"]
-
-    control_dim, n_samples = _grid_minimization(
-        _cost, (control_dim_float, n_samples_float), grid_search_width, constraint
-    )
-
-    return control_dim, n_samples
 
 
-def OptMLESinQPE_params(target_error, damp_strength=0.0, grid_search_width=5):
-    thresh_dim1 = int(DEPTH_FACTOR_1 * damp_strength ** (-1 / 3)) + 1
-    thresh_dim2 = int(DEPTH_FACTOR_2 / damp_strength) + 1
-
-    thresh_error1 = SinQPE_Holevo_error(thresh_dim1, damp_strength)
-    thresh_error2 = 1 / np.sqrt(SinQPE_FI(thresh_dim2, damp_strength) * MAX_NSHOT)
-
-    if target_error > thresh_error1:
-        n_samples = 1
-        control_dim = 1 + np.ceil(np.pi / target_error).astype(int)
-    elif target_error < thresh_error2:
-        n_samples = np.ceil(1 / SinQPE_FI(thresh_dim2, damp_strength) / target_error**2).astype(int)
-        control_dim = thresh_dim2
-    else:
-        control_dim, n_samples = _OptMLESinQPE_midregime(
-            target_error,
-            damp_strength,
-            thresh_dim1,
-            thresh_dim2,
-            grid_search_width=grid_search_width,
-        )
-
-    return int(control_dim), int(n_samples)
